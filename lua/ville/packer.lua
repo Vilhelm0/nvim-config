2419dc2d5653f8efe399ea77cf5f46598498feaf
Vilhelm <v.eriksson@allgon.com>
2023-03-31 17:41:00 +0200

    added .gitignore and added added neogen plugin to packer

--- lua/ville/packer.lua
+++ lua/ville/packer.lua
@@ -70,6 +70,19 @@ use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
  
 
 	  use('tpope/vim-fugitive')
+
+      use {
+          "danymat/neogen",
+          config = function()
+              require('neogen').setup ({snippet_engine = "luasnip"})
+          end,
+          requires = "nvim-treesitter/nvim-treesitter",
+          -- Uncomment next line if you want to follow only stable versions
+          tag = "*"
+      }
+
+
+
       
 end)
 
